4b940dd1872fc0c101d0e43422687b712cf3b7f0
Prashan5903s <chaubeyprashant498@gmail.com>
2026-08-26 11:32:23 +0530

    Changes of config

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -12,7 +12,7 @@ class CorsMiddleware
         $origin = $request->header('Origin');
 
         $allowedOrigins = [
-            'https://uat-eld.vercel.app0'
+            'https://uat-eld.vercel.app'
         ];
 
         if ($request->isMethod('OPTIONS')) {
