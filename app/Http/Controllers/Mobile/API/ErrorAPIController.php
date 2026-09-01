7eb60b3beb8487fe6b063249a5013338377e13d8
Prashan5903s <chaubeyprashant498@gmail.com>
2026-09-01 15:28:26 +0530

    Changes of error log body

--- app/Http/Controllers/Mobile/API/ErrorAPIController.php
+++ app/Http/Controllers/Mobile/API/ErrorAPIController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Mobile\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Models\ErrorLog;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class ErrorAPIController extends Controller
 {
@@ -14,18 +14,19 @@ class ErrorAPIController extends Controller
     public function error_data_save(Request $request)
     {
 
-        
 
-        $validator = Validator::make($request->all(), [
-            'device_platform' => 'required|string|in:android,ios',
-            'log_file' => 'required|file|max:10240|mimetypes:text/plain,text/x-log,application/octet-stream',
-        ]);
 
-        if ($validator->fails()) {
+        try {
+            $request->validate([
+                'device_platform' => 'required|string|in:android,ios',
+                'log_file' => 'required|file|max:10240|mimetypes:text/plain,text/x-log,application/octet-stream',
+            ]);
+        } catch (ValidationException $e) {
             return response()->json([
-                'status' => false,
-                'message' => 'Validation failed.',
-                'errors' => $validator->errors(),
+                'status' => 'failure',
+                'statusCode' => 422,
+                'message' => 'Validation failed',
+                'errors' => $e->errors(),
             ], 422);
         }
 
