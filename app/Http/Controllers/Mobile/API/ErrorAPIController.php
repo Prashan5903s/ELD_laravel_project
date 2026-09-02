1c42b7d0484e58db016514368485742b89f0430e
Prashan5903s <chaubeyprashant498@gmail.com>
2026-09-02 16:02:55 +0530

    Changes

--- app/Http/Controllers/Mobile/API/ErrorAPIController.php
+++ app/Http/Controllers/Mobile/API/ErrorAPIController.php
@@ -21,13 +21,13 @@ class ErrorAPIController extends Controller
                 'log_file' => 'required|file',
             ]);
 
-            // Check uploaded file
-            if (!$request->hasFile('log_file') || !$request->file('log_file')->isValid()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Invalid log file uploaded.',
-                ], 400);
-            }
+            // // Check uploaded file
+            // if (!$request->hasFile('log_file') || !$request->file('log_file')->isValid()) {
+            //     return response()->json([
+            //         'status' => false,
+            //         'message' => 'Invalid log file uploaded.',
+            //     ], 400);
+            // }
 
             Log::info([
                 'auth_check' => Auth::check(),
