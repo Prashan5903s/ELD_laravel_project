3ef39264039f9c5a60e4965d155850045ee05dad
Prashan5903s <chaubeyprashant498@gmail.com>
2026-09-02 16:04:37 +0530

    Changes

--- routes/mobileAPI.php
+++ routes/mobileAPI.php
@@ -33,7 +33,7 @@ Route::post('reset/mobile/password/{email}', [UserMobileAPIController::class, 's
 // Protected routes (requires mobileAPI guard)
 Route::middleware(['auth:mobileAPI', 'DrCheckMobile', 'mobileAPI'])->group(function () {
 
-    Route::post("/error/data/log", [ErrorAPIController::class, 'error_data_save']);
+    Route::post("error/data/log", [ErrorAPIController::class, 'error_data_save']);
 
     Route::get('config/data', [AppConfigAPIController::class, 'app_config_data']);
 
