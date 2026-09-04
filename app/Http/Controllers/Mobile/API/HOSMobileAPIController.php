d2b29ebfba64e6fb6ab5285b4f3487e148af77b7
Prashan5903s <chaubeyprashant498@gmail.com>
2026-09-04 12:20:29 +0530

    Changes of edit log API

--- app/Http/Controllers/Mobile/API/HOSMobileAPIController.php
+++ app/Http/Controllers/Mobile/API/HOSMobileAPIController.php
@@ -815,11 +815,11 @@ class HOSMobileAPIController extends Controller
                 'log_data.*.log_id' => 'nullable|integer',
                 'log_data.*.start_log_time' => 'required|date_format:H:i:s',
                 'log_data.*.end_log_time' => 'required|date_format:H:i:s',
-                'log_data.*.location_start' => 'required|string|max:255',
+                'log_data.*.location_start' => 'nullable|string|max:255',
                 'log_data.*.location_end' => 'nullable|string|max:255',
                 'log_data.*.odometer' => 'nullable|string|max:225',
                 'log_data.*.engine_hour' => 'nullable|string|max:255',
-                'log_data.*.notes' => 'required|string|max:1000',
+                'log_data.*.notes' => 'nullable|string|max:1000',
             ]);
 
         } catch (ValidationException $e) {
